20da5de37ee712a71fa11c172d5fca1744d979ac
Ntholi Nkhatho <ntholi@live.com>
2023-06-28 10:49:20 +0200

    Add Mantine

---
+++ src/app/(profile)/profile/components/Sample.tsx
@@ -0,0 +1,3 @@
+export default function Sample() {
+  return <h1>Sample</h1>;
+}
---
+++ src/app/(profile)/profile/page.tsx
@@ -0,0 +1,9 @@
+import Sample from './components/Sample';
+
+export default function ProfilePage() {
+  return (
+    <>
+      <Sample />
+    </>
+  );
+}
