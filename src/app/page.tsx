5a3052fdb056496f27ff1a6dc462502cdfacfb2f
Ntholi Nkhatho <ntholi@live.com>
2023-08-03 08:14:19 +0200

    Fix "Type AuthOptions is not assignable to type never"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth';
-import { authOptions } from './api/auth/[...nextauth]/route';
+import { authOptions } from './api/auth/[...nextauth]/auth';
 
 async function fetchDatetime() {
   const res = await fetch('http://worldtimeapi.org/api/timezone/Africa/Maseru');
