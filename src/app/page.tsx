e81d5714d3d6f68303a497ac774063059daa2059
Ntholi Nkhatho <ntholi@live.com>
2023-08-09 20:18:57 +0200

    delete dummy text in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,8 @@ import { Button } from '@nextui-org/button';
 
 export default function Page() {
   return (
-    <div>
-      <Button color="primary">Button</Button>
-      {Array.from({ length: 100 }).map((_, i) => (
-        <p key={i}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum numquam
-          voluptatum minus laborum nulla placeat reiciendis eveniet omnis
-          repudiandae praesentium asperiores sint maxime reprehenderit
-          consequuntur, consequatur quas consectetur culpa provident.
-        </p>
-      ))}
+    <div className="h-[80vh] flex justify-center items-center">
+      <h1 className="text-6xl">PhotoBank</h1>
     </div>
   );
 }
