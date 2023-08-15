96f6445d8154ac7830d6c73c60990f53fcfe2577
Ntholi Nkhatho <ntholi@live.com>
2023-08-15 11:43:35 +0200

    init upload page

--- src/app/profile/components/ProfileNav.tsx
+++ src/app/profile/components/ProfileNav.tsx
@@ -1,23 +1,15 @@
 'use client';
 import { useSession } from '@/lib/context/UserContext';
-import {
-  Avatar,
-  Button,
-  Divider,
-  Modal,
-  useDisclosure,
-} from '@nextui-org/react';
+import { Avatar, Button, Divider } from '@nextui-org/react';
 import Image from 'next/image';
-import Link from 'next/link';
+import NextLink from 'next/link';
 import React from 'react';
 import { GoBell, GoGear, GoHome, GoUpload } from 'react-icons/go';
 import { useIsMobile } from '@/lib/hooks/useIsMobile';
-import UploadModel from './upload/UploadModel';
 
 export default function ProfileNav() {
   const { user } = useSession();
   const isMobile = useIsMobile();
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const navItems = [
     { name: 'Home', link: `/`, icon: <GoHome /> },
@@ -37,35 +29,31 @@ export default function ProfileNav() {
     },
   ];
   return (
-    <>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <UploadModel />
-      </Modal>
-      <nav className="bg-background fixed bottom-0 w-full sm:w-1/4 sm:h-screen sm:static border-t border-t-zinc-100 sm:border-e sm:border-t-0 border-zinc-300">
-        <div className="p-8 max-sm:hidden">
-          <Image alt="logo" src="/images/logo.jpg" width={100} height={100} />
+    <nav className="bg-background fixed bottom-0 w-full sm:w-1/4 sm:h-screen sm:static border-t border-t-zinc-100 sm:border-e sm:border-t-0 border-zinc-300">
+      <div className="p-8 max-sm:hidden">
+        <Image alt="logo" src="/images/logo.jpg" width={100} height={100} />
+      </div>
+      <ul className="flex justify-evenly sm:block">
+        {navItems.map((item) => (
+          <NevItem key={item.name} {...item} />
+        ))}
+        <div className="p-5 max-md:hidden">
+          <Divider />
         </div>
-        <ul className="flex justify-evenly sm:block">
-          {navItems.map((item) => (
-            <NevItem key={item.name} {...item} />
-          ))}
-          <div className="p-5 max-md:hidden">
-            <Divider />
-          </div>
-          <li className="p-2 md:ps-6 md:pe-10 max-md:text-center">
-            <Button
-              startContent={!isMobile && <GoUpload />}
-              variant="ghost"
-              isIconOnly={isMobile}
-              className="md:px-8 border-1.5 rounded-full md:rounded-md"
-              onPress={onOpen}
-            >
-              {isMobile ? <GoUpload /> : 'Upload'}
-            </Button>
-          </li>
-        </ul>
-      </nav>
-    </>
+        <li className="p-2 md:ps-6 md:pe-10 max-md:text-center">
+          <Button
+            startContent={!isMobile && <GoUpload />}
+            variant="ghost"
+            isIconOnly={isMobile}
+            href={`/profile/${user?.uid}/upload`}
+            as={NextLink}
+            className="md:px-8 border-1.5 rounded-full md:rounded-md"
+          >
+            {isMobile ? <GoUpload /> : 'Upload'}
+          </Button>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
@@ -80,14 +68,14 @@ const NevItem = ({ name, icon, link, active }: NavItemProps) => {
   const activeClass = active ? 'text-black' : 'text-zinc-600';
   return (
     <li key={name} className="group p-2">
-      <Link
+      <NextLink
         href={link}
         className={`p-2 w-full relative rounded-sm flex md:justify-start justify-center items-center ${activeClass} border-s-2 border-transparent sm:hover:border-zinc-800 hover:text-zinc-800 hover:bg-zinc-50`}
       >
         <span className="mx-3 text-2xl">{icon}</span>
         <span className="hidden md:inline text-lg font-semibold">{name}</span>
         {/* <PiArrowRightBold className="absolute right-5 group-hover:inline group-hover:translate-x-3 transition-transform group-hover:text-zinc-500 text-zinc-200" /> */}
-      </Link>
+      </NextLink>
     </li>
   );
 };
