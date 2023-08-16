39873464b4af4ce0a33d6945037f30185f422ac1
Ntholi Nkhatho <ntholi@live.com>
2023-08-16 14:36:29 +0200

    add file input in UploadModal

--- src/app/profile/[id]/upload/components/UploadModal.tsx
+++ src/app/profile/[id]/upload/components/UploadModal.tsx
@@ -8,39 +8,43 @@ import {
   useDisclosure,
 } from '@nextui-org/modal';
 import { Button } from '@nextui-org/button';
+import { GoUpload } from 'react-icons/go';
+import { BiSolidCloudUpload } from 'react-icons/bi';
+import { useIsMobile } from '@/lib/hooks/useIsMobile';
 
 type Props = {
   isOpen: boolean;
   onOpenChange: (open: boolean) => void;
 };
 export default function UploadModal({ isOpen, onOpenChange }: Props) {
+  const fileRef = React.useRef<HTMLInputElement>(null);
+  const isMobile = useIsMobile();
   return (
-    <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+    <Modal
+      size={isMobile ? 'full' : '2xl'}
+      backdrop="blur"
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+    >
       <ModalContent>
         {(onClose) => (
           <>
             <ModalHeader className="flex flex-col gap-1">
-              Modal Title
+              Upload Photo
             </ModalHeader>
-            <ModalBody>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                pulvinar risus non risus hendrerit venenatis. Pellentesque sit
-                amet hendrerit risus, sed porttitor quam.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                pulvinar risus non risus hendrerit venenatis. Pellentesque sit
-                amet hendrerit risus, sed porttitor quam.
-              </p>
-              <p>
-                Magna exercitation reprehenderit magna aute tempor cupidatat
-                consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                aliqua enim laboris do dolor eiusmod. Et mollit incididunt nisi
-                consectetur esse laborum eiusmod pariatur proident Lorem eiusmod
-                et. Culpa deserunt nostrud ad veniam.
-              </p>
+            <ModalBody className={'md:px-10 md:py-5'}>
+              <input type="file" name="picture" ref={fileRef} hidden />
+              <div
+                className={
+                  'w-full h-72 border border-gray-300 rounded-xl flex flex-col justify-center items-center cursor-pointer'
+                }
+                onClick={() => fileRef.current?.click()}
+              >
+                <BiSolidCloudUpload size="3rem" />
+                <p className="text-sm text-gray-500">
+                  Click here to upload an image
+                </p>
+              </div>
             </ModalBody>
             <ModalFooter>
               <Button color="danger" variant="light" onClick={onClose}>
