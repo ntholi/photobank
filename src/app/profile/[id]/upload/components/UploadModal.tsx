e3d56606fd62fb6a51c38189bb3cc43234e83417
Ntholi Nkhatho <ntholi@live.com>
2023-08-16 13:34:34 +0200

    Init UploadModal

---
+++ src/app/profile/[id]/upload/components/UploadModal.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from '@nextui-org/modal';
+import { Button } from '@nextui-org/button';
+
+type Props = {
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
+};
+export default function UploadModal({ isOpen, onOpenChange }: Props) {
+  return (
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">
+              Modal Title
+            </ModalHeader>
+            <ModalBody>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                pulvinar risus non risus hendrerit venenatis. Pellentesque sit
+                amet hendrerit risus, sed porttitor quam.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                pulvinar risus non risus hendrerit venenatis. Pellentesque sit
+                amet hendrerit risus, sed porttitor quam.
+              </p>
+              <p>
+                Magna exercitation reprehenderit magna aute tempor cupidatat
+                consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
+                incididunt cillum quis. Velit duis sit officia eiusmod Lorem
+                aliqua enim laboris do dolor eiusmod. Et mollit incididunt nisi
+                consectetur esse laborum eiusmod pariatur proident Lorem eiusmod
+                et. Culpa deserunt nostrud ad veniam.
+              </p>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="danger" variant="light" onClick={onClose}>
+                Close
+              </Button>
+              <Button color="primary" onPress={onClose}>
+                Action
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+}
--- src/app/profile/[id]/upload/page.tsx
+++ src/app/profile/[id]/upload/page.tsx
@@ -1,23 +1,35 @@
+'use client';
 import React from 'react';
 import { Image } from '@nextui-org/image';
 import { Button } from '@nextui-org/button';
 import { GoUpload } from 'react-icons/go';
+import { useDisclosure } from '@nextui-org/modal';
+import UploadModal from './components/UploadModal';
 
 export default function UploadPage() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
-    <section className="flex flex-col justify-center items-center w-full h-screen">
-      <Image src="/images/photo_session.svg" alt="photo upload " width={400} />
-      <h1 className="text-lg font-semibold text-gray-600 mt-6">
-        Upload Your Photo
-      </h1>
-      <p className="text-sm text-gray-400 sm:w-96 text-center">
-        Please note that only photos related to Lesotho bla-bla will be
-        accepted, all photos are subject to review before they can be published
-        on the photo bank
-      </p>
-      <Button startContent={<GoUpload />} className="mt-6">
-        Upload
-      </Button>
-    </section>
+    <>
+      <UploadModal isOpen={isOpen} onOpenChange={onOpenChange} />
+      <section className="flex flex-col justify-center items-center w-full h-screen">
+        <Image
+          src="/images/photo_session.svg"
+          alt="photo upload "
+          width={400}
+        />
+        <h1 className="text-2xl font-semibold text-gray-700 mt-8">
+          Upload Your Photo
+        </h1>
+        <p className="text-sm text-gray-500 sm:w-96 text-center my-4">
+          Please note that only photos related to Lesotho bla-bla will be
+          accepted, all photos are subject to review before they can be
+          published on the photo bank
+        </p>
+        <Button onPress={onOpen} startContent={<GoUpload />} className="">
+          Upload
+        </Button>
+      </section>
+    </>
   );
 }
