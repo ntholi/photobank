75d1ca5c2bf70bd4fe48ebe37c9606b5fc8c26cf
Ntholi Nkhatho <ntholi@live.com>
2023-08-17 10:33:59 +0200

    redo db migrations

--- prisma/migrations/20230817083300_initial_commit/migration.sql
+++ prisma/migrations/20230817083300_initial_commit/migration.sql
@@ -1,6 +1,19 @@
 -- CreateTable
-CREATE TABLE "photos" (
+CREATE TABLE "users" (
     "id" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "email" TEXT,
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "photos" (
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT,
     "location" TEXT,
@@ -11,6 +24,9 @@ CREATE TABLE "photos" (
 
     CONSTRAINT "photos_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
 ALTER TABLE "photos" ADD CONSTRAINT "photos_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
