94b068e2a78b534c7cea7fc15b1a67e43b6b4bc6
Ntholi Nkhatho <ntholi@live.com>
2023-08-17 17:22:53 +0200

    Make admin nav responsive

--- src/app/admin/components/AdminNav.tsx
+++ src/app/admin/components/AdminNav.tsx
@@ -1,21 +1,25 @@
 'use client';
 import Logo from '@/app/base/Logo';
+import Link from 'next/link';
 import NextLink from 'next/link';
 import React from 'react';
-import { GoImage, GoPerson } from 'react-icons/go';
+import { GoHome, GoImage, GoPerson } from 'react-icons/go';
 
 export default function ProfileNav() {
   const navItems = [
-    { name: 'Users', link: `#`, icon: <GoPerson /> },
+    { name: 'Home', link: `/`, icon: <GoHome /> },
     { name: 'Photos', link: `#`, icon: <GoImage /> },
+    { name: 'Users', link: `#`, icon: <GoPerson /> },
   ];
   return (
     <nav className="z-20 bg-background fixed bottom-0 w-full sm:w-[20vw] sm:h-screen border-t border-t-zinc-100 sm:border-e sm:border-t-0 border-zinc-300">
-      <div className="flex items-center gap-5 p-5 border-b border-zinc-300 bg-gray-100 ">
-        <Logo size={70} />
-        <h1 className="text-2xl font-semibold uppercase font-mono">Admin</h1>
+      <div className="hidden sm:flex items-center gap-5 p-5 border-b border-zinc-300 bg-gray-100 ">
+        <Link href={'/'}>
+          <Logo size={70} />
+        </Link>
+        <h1 className="md:text-2xl font-semibold uppercase font-mono">Admin</h1>
       </div>
-      <ul className="flex justify-evenly sm:block">
+      <ul className="mt-3 flex justify-evenly sm:block">
         {navItems.map((item) => (
           <NevItem key={item.name} {...item} />
         ))}
@@ -34,13 +38,13 @@ type NavItemProps = {
 const NevItem = ({ name, icon, link, active }: NavItemProps) => {
   const activeClass = active ? 'text-black' : 'text-zinc-600';
   return (
-    <li key={name} className="group p-2">
+    <li key={name} className="group px-2">
       <NextLink
         href={link}
-        className={`p-2 w-full relative rounded-sm flex lg:justify-start justify-center items-center ${activeClass} border border-transparent sm:hover:border-zinc-200 hover:text-zinc-800 hover:bg-zinc-50`}
+        className={`p-4 w-full relative rounded-sm flex lg:justify-start justify-center items-center ${activeClass} border border-transparent sm:hover:border-zinc-200 hover:text-zinc-800 hover:bg-zinc-50`}
       >
         <span className="mx-3 text-2xl">{icon}</span>
-        <span className="hidden lg:inline text-lg font-semibold">{name}</span>
+        <span className="hidden lg:inline font-semibold">{name}</span>
       </NextLink>
     </li>
   );
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,8 +2,7 @@ export default function ProfilePage() {
   return (
     <main className="flex h-screen justify-center items-center ">
       <div>
-        <h1 className="text-8xl text-center">PhotBank</h1>
-        <p className="text-xl text-center mt-4 uppercase">
+        <p className="text-xl text-center mt-4 uppercase text-gray-800 font-bold">
           Imagine a very nice admin page here
         </p>
       </div>
