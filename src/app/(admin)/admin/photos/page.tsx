da64af1f4e2d4f6c241945b73364dbdec495f0d8
Ntholi Nkhatho <ntholi@live.com>
2023-08-18 09:59:43 +0200

    add search field to admin tables

--- src/app/(admin)/admin/photos/page.tsx
+++ src/app/(admin)/admin/photos/page.tsx
@@ -1,13 +1,14 @@
-import { ActionIcon, Paper, Space, Table, Title } from '@mantine/core';
-import { IconReload } from '@tabler/icons-react';
+import { ActionIcon, Paper, Space, Input, Title, Group } from '@mantine/core';
+import { IconReload, IconSearch } from '@tabler/icons-react';
 import React from 'react';
 import PhotosTable from './PhotosTable';
 import { Photo } from '@prisma/client';
+import axios from 'axios';
 import { api } from '@/lib/constants';
 
 const fetchPhotos = async () => {
-  const response = await fetch(api('photos'));
-  return (await response.json()) as Photo[];
+  const res = await axios.get(api('photos'));
+  return res.data as Photo[];
 };
 
 export default async function PhotosPage() {
@@ -16,9 +17,15 @@ export default async function PhotosPage() {
     <div>
       <Paper withBorder p="lg">
         <header className="flex items-center justify-between">
-          <Title order={1} size={'h5'}>
-            Photos
-          </Title>
+          <Group>
+            <Title order={1} size={'h5'}>
+              Photos
+            </Title>
+            <Input
+              placeholder="Search"
+              rightSection={<IconSearch size="1.125rem" />}
+            />
+          </Group>
           <ActionIcon
             size={'lg'}
             variant="filled"
