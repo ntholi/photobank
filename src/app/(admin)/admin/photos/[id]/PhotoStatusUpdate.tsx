aebc3746eff490161c5348ed27f7974343bbf131
Ntholi Nkhatho <ntholi@live.com>
2023-08-18 16:26:52 +0200

    replace Chip with Button in PhotoStatusUpdate

--- src/app/(admin)/admin/photos/[id]/PhotoStatusUpdate.tsx
+++ src/app/(admin)/admin/photos/[id]/PhotoStatusUpdate.tsx
@@ -1,38 +1,53 @@
 'use client';
 import FieldDisplay from '@/app/(admin)/base/FieldDisplay';
-import { Badge, Chip, Group } from '@mantine/core';
-import React from 'react';
+import { Badge, Button } from '@mantine/core';
+import React, { useState } from 'react';
 import { Photo, PhotoStatus } from '@prisma/client';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
 
+const items = [
+  {
+    label: 'Approve',
+    value: 'approved',
+  },
+  {
+    label: 'Reject',
+    value: 'rejected',
+  },
+  {
+    label: 'Pending',
+    value: 'pending',
+  },
+] as const;
+
 export default function PhotoStatusUpdate({ photo }: { photo: Photo }) {
-  const items = ['pending', 'approved', 'rejected'];
   const router = useRouter();
+  const [loading, setLoading] = useState<string | null>(null);
 
   function handleSelect(item: PhotoStatus) {
-    axios.put(`/api/photos/${photo.id}`, { status: item });
+    setLoading(item);
+    axios
+      .put(`/api/photos/${photo.id}`, { status: item })
+      .finally(() => setLoading(null));
     router.refresh();
   }
 
   return (
     <FieldDisplay label="Status">
       <StatusDisplay status={photo.status} />
-      <div className="mt-1">
-        <Chip.Group>
-          <Group justify="center">
-            {items.map((item) => (
-              <Chip
-                key={item}
-                value={item}
-                defaultChecked={item === status}
-                onClick={() => handleSelect(item as PhotoStatus)}
-              >
-                {item}
-              </Chip>
-            ))}
-          </Group>
-        </Chip.Group>
+      <div className="mt-2 space-x-2">
+        {items.map((item) => (
+          <Button
+            key={item.value}
+            size="xs"
+            variant="default"
+            onClick={() => handleSelect(item.value)}
+            loading={loading === item.value}
+          >
+            {item.label}
+          </Button>
+        ))}
       </div>
     </FieldDisplay>
   );
@@ -53,5 +68,9 @@ export const StatusDisplay = ({ status }: { status: PhotoStatus }) => {
     default:
       color = 'gray';
   }
-  return <Badge color={color}>{status}</Badge>;
+  return (
+    <Badge color={color} variant="outline">
+      {status}
+    </Badge>
+  );
 };
