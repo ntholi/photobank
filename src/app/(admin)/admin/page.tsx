764e6c00667d8f79d5c5690cf45894814af5d332
Ntholi Nkhatho <ntholi@live.com>
2023-08-18 20:14:16 +0200

    make tailwind override mantine stiles

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link';
-import { Button } from '@mantine/core';
+import { Button, Title } from '@mantine/core';
 
 export default function AdminPage() {
   return (
-    <Button className="m-5" component={Link} href="#">
-      Next link button
-    </Button>
+    <div className="flex items-center justify-center h-[85vh]">
+      <Title className="text-2xl text-center font-mono uppercase" c={'gray'}>
+        Image an Admin Dashboard Here
+      </Title>
+    </div>
   );
 }
--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,5 +1,5 @@
-import '../globals.css';
 import '@mantine/core/styles.css';
+import '../globals.css';
 
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { APP_NAME } from '@/lib/constants';
