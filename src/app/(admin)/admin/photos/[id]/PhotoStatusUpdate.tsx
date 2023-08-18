5b9ebc75747a5e598857e567eef6748f31e990ee
Ntholi Nkhatho <ntholi@live.com>
2023-08-18 15:54:41 +0200

    handle photo status update in api

--- src/app/(admin)/admin/photos/[id]/PhotoStatusUpdate.tsx
+++ src/app/(admin)/admin/photos/[id]/PhotoStatusUpdate.tsx
@@ -1,12 +1,39 @@
+'use client';
 import FieldDisplay from '@/app/(admin)/base/FieldDisplay';
-import { Badge, Chip } from '@mantine/core';
+import { Badge, Chip, Group } from '@mantine/core';
 import React from 'react';
 import { Photo, PhotoStatus } from '@prisma/client';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
 export default function PhotoStatusUpdate({ photo }: { photo: Photo }) {
+  const items = ['pending', 'approved', 'rejected'];
+  const router = useRouter();
+
+  function handleSelect(item: PhotoStatus) {
+    axios.put(`/api/photos/${photo.id}`, { status: item });
+    router.refresh();
+  }
+
   return (
     <FieldDisplay label="Status">
       <StatusDisplay status={photo.status} />
+      <div className="mt-1">
+        <Chip.Group>
+          <Group justify="center">
+            {items.map((item) => (
+              <Chip
+                key={item}
+                value={item}
+                defaultChecked={item === status}
+                onClick={() => handleSelect(item as PhotoStatus)}
+              >
+                {item}
+              </Chip>
+            ))}
+          </Group>
+        </Chip.Group>
+      </div>
     </FieldDisplay>
   );
 }
--- src/app/api/photos/[id]/route.ts
+++ src/app/api/photos/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/db';
+import { Photo } from '@prisma/client';
 
 type Props = {
     params: {
@@ -13,6 +14,27 @@ export async function GET(request: Request, { params }: Props) {
     const photo = await prisma.photo.findUnique({
         where: {
             id: id,
+        },
+    });
+    if (!photo) {
+        throw new Error(`Photo with id ${id} not found`);
+    }
+    return NextResponse.json(photo);
+}
+
+export async function PUT(request: Request, { params }: Props) {
+    const id = Number(params.id || -1);
+
+    if (id < 0) {
+        throw new Error(`Invalid id ${id}`);
+    }
+    const data = (await request.json()) as Photo;
+    const photo = await prisma.photo.update({
+        where: {
+            id: id,
+        },
+        data: {
+            ...data,
         },
     });
     if (!photo) {
