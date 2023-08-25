a229f88205e5bdd1bef2d3a87e6ea1a7b2426c9e
Ntholi Nkhatho <ntholi@live.com>
2023-08-25 21:03:30 +0200

    init firebase auth

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -1,12 +1,21 @@
 import { prisma } from '@/lib/db';
 import { AuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@auth/prisma-adapter';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import bcrypt from 'bcrypt';
+import { FirestoreAdapter } from '@auth/firebase-adapter';
+import { cert } from 'firebase-admin/app';
 
 export const authOptions = {
-    adapter: PrismaAdapter(prisma) as any,
+    // adapter: FirestoreAdapter({
+    //     credential: cert({
+    //         projectId: process.env.FIREBASE_PROJECT_ID,
+    //         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    //         privateKey: process.env.FIREBASE_PRIVATE_KEY
+    //             ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    //             : undefined,
+    //     }),
+    // }),
     secret: process.env.SECRET,
     session: {
         strategy: 'jwt',
@@ -55,22 +64,22 @@ export const authOptions = {
             },
         }),
     ],
-    callbacks: {
-        async jwt({ token, account, profile, user }) {
-            if (user) {
-                token.id = user.id;
-                token.username = user.username;
-                token.role = user.role;
-            }
-            return token;
-        },
-        async session({ session, token, user }) {
-            if (session.user) {
-                session.user.id = token.id;
-                session.user.username = token.username;
-                session.user.role = token.role;
-            }
-            return session;
-        },
-    },
+    // callbacks: {
+    //     async jwt({ token, account, profile, user }) {
+    //         if (user) {
+    //             token.id = user.id;
+    //             token.username = user.username;
+    //             token.role = user.role;
+    //         }
+    //         return token;
+    //     },
+    //     async session({ session, token, user }) {
+    //         if (session.user) {
+    //             session.user.id = token.id;
+    //             session.user.username = token.username;
+    //             session.user.role = token.role;
+    //         }
+    //         return session;
+    //     },
+    // },
 } as AuthOptions;
