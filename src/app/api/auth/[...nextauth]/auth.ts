6a10f3bbf71c0bbe15569d2b0a37e96b53b7355e
Ntholi Nkhatho <ntholi@live.com>
2023-08-27 17:36:02 +0200

    add facebook auth provider

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -2,6 +2,7 @@ import { prisma } from '@/lib/db';
 import { AuthOptions, User } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import FacebookProvider from 'next-auth/providers/facebook';
 import { auth } from '@/lib/config/firebase';
 import {
     GoogleAuthProvider,
@@ -15,12 +16,17 @@ export const authOptions = {
     session: {
         strategy: 'jwt',
     },
-    // debug: process.env.NODE_ENV === 'development',
+    debug: process.env.NODE_ENV === 'development',
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || '',
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
         }),
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_CLIENT_ID || '',
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET || '',
+        }),
+
         CredentialsProvider({
             name: 'Credentials',
             credentials: {
