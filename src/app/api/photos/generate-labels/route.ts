e33b3f5c425bd56fc82506b4391a1beb7101e215
Ntholi Nkhatho <ntholi@live.com>
2023-09-12 01:06:27 +0200

    etry 3 times if no labels found

--- src/app/api/photos/generate-labels/route.ts
+++ src/app/api/photos/generate-labels/route.ts
@@ -7,5 +7,15 @@ export async function GET(req: Request) {
 
     const labels = await getLabels(photoUrl);
 
+    // retry 3 times if no labels found
+    if (!labels.length) {
+        for (let i = 0; i < 3; i++) {
+            const labels = await getLabels(photoUrl);
+            if (labels.length) {
+                break;
+            }
+        }
+    }
+
     return NextResponse.json({ labels });
 }
