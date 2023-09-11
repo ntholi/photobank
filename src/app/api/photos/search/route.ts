2b1106e6c485794ff0a06ce7da8cd3412f038ce5
Ntholi Nkhatho <ntholi@live.com>
2023-09-11 18:29:12 +0200

    Filtering by label works

--- src/app/api/photos/search/route.ts
+++ src/app/api/photos/search/route.ts
@@ -12,6 +12,16 @@ export async function POST(req: Request) {
         console.error('Error parsing request body');
     }
 
+    const labels = await prisma.label.findMany({
+        where: {
+            tags: {
+                some: {
+                    id: tagId,
+                },
+            },
+        },
+    });
+
     const data = await prisma.photo.findMany({
         where: {
             status: 'published',
@@ -19,11 +29,13 @@ export async function POST(req: Request) {
                 contains: searchKey ? searchKey : '',
                 mode: 'insensitive',
             },
-            // tags: {
-            //     some: {
-            //         tagId: tagId,
-            //     },
-            // },
+            labels: {
+                some: {
+                    labelId: {
+                        in: labels.map((it) => it.id),
+                    },
+                },
+            },
         },
         include: {
             user: true,
