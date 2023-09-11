b5053ff34957599b7bf2727f62a31809a6bbc92c
Ntholi Nkhatho <ntholi@live.com>
2023-09-11 22:59:23 +0200

    init SimilarPhotos

---
+++ src/app/(main)/photos/[id]/SimilarPhotos.tsx
@@ -0,0 +1,70 @@
+import { prisma } from '@/lib/db';
+import { PhotoWithData } from '@/lib/types';
+import React from 'react';
+import { Image } from '@nextui-org/image';
+import Link from 'next/link';
+
+type Props = {
+  photo: PhotoWithData;
+};
+
+const getSimilarPhotos = async (photoId: string) => {
+  const photo = await prisma.photo.findUnique({
+    where: { id: photoId },
+    include: {
+      labels: true,
+    },
+  });
+  const labelIds = photo?.labels.map((label) => label.labelId);
+
+  const data = await prisma.photo.findMany({
+    where: {
+      status: 'published',
+      labels: {
+        some: {
+          labelId: {
+            in: labelIds?.map((it) => it),
+          },
+        },
+      },
+      id: {
+        not: photoId,
+      },
+    },
+  });
+
+  const photos = data.map((it) => {
+    const fileName = it.fileName.split('.')[0];
+    return {
+      ...it,
+      url: `https://djvt9h5y4w4rn.cloudfront.net/${fileName}-thumb.jpg`,
+    };
+  });
+
+  return photos as PhotoWithData[];
+};
+
+export default async function SimilarPhotos({ photo }: Props) {
+  const photos = await getSimilarPhotos(photo.id);
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        {photos.map((photo: PhotoWithData) => (
+          <Link
+            className="relative flex flex-1"
+            key={photo.id}
+            href={`/photos/${photo.id}`}
+          >
+            <Image
+              className={'min-w-full h-60 sm:h-72 object-cover cursor-pointer'}
+              src={photo.url}
+              alt={photo.caption || 'Lesotho'}
+              height={900}
+              width={900}
+            />
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+}
--- src/app/(main)/photos/[id]/page.tsx
+++ src/app/(main)/photos/[id]/page.tsx
@@ -3,6 +3,7 @@ import { prisma } from '@/lib/db';
 import { Image } from '@nextui-org/image';
 import DetailsCard from './DetailsCard';
 import { PhotoWithData } from '@/lib/types';
+import SimilarPhotos from './SimilarPhotos';
 
 type Props = { params: { id: string } };
 
@@ -32,17 +33,23 @@ export default async function Page({ params }: Props) {
   }
 
   return (
-    <div className="bg-gray-50 ">
+    <>
+      <div className="bg-gray-50 ">
+        <section className="container mx-auto px-4 py-10">
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-10">
+            <div className="md:col-span-3">
+              <Image src={photo.url} alt={photo.caption || 'Lesotho'} />
+            </div>
+            <div className="md:col-span-2">
+              <DetailsCard photo={photo} />
+            </div>
+          </div>
+        </section>
+      </div>
       <section className="container mx-auto px-4 py-10">
-        <div className="grid grid-cols-1 md:grid-cols-5 gap-10">
-          <div className="md:col-span-3">
-            <Image src={photo.url} alt={photo.caption || 'Lesotho'} />
-          </div>
-          <div className="md:col-span-2">
-            <DetailsCard photo={photo} />
-          </div>
-        </div>
+        <h2>Similar Photos</h2>
+        <SimilarPhotos photo={photo} />
       </section>
-    </div>
+    </>
   );
 }
