0e77117775874826fd91e234313760794c246fbb
Ntholi Nkhatho <ntholi@live.com>
2023-09-13 12:37:30 +0200

    add Maintain component

---
+++ src/app/(main)/Mantain.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Maintain() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center max-w-lg">
+        <div className="mb-4 flex flex-col justify-center items-center">
+          <img src="/images/logo.jpg" alt="Logo" className="my-4 w-36" />
+          <h1 className="text-5xl font-extrabold text-blue-600">503</h1>
+        </div>
+        <h3 className="mb-3 text-2xl font-bold text-center text-gray-700">
+          Temporarily down for maintenance We’ll be back soon!
+        </h3>
+        <p className="text-sm text-center text-gray-600">
+          Sorry for the inconvenience but we’re performing some maintenance at
+          the moment. If you need to you can always{' '}
+          <a className="text-blue-600 hover:underline">Contact us </a>,
+          otherwise we’ll be back online shortly!
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import '../globals.css';
 import { Inter } from 'next/font/google';
 import { Providers } from './providers';
 import Navbar from './base/Navbar';
+import Maintain from './Mantain';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,10 +15,7 @@ export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
-          <Navbar />
-          {children}
-        </Providers>
+        <Maintain />
       </body>
     </html>
   );
