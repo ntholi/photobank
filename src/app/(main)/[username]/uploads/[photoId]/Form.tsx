0223ef0df845e2c7098564f195ae1a864d746ffd
Ntholi Nkhatho <ntholi@live.com>
2023-09-16 14:50:25 +0200

    create checkbox for "Allow for homepage display"

--- src/app/(main)/[username]/uploads/[photoId]/Form.tsx
+++ src/app/(main)/[username]/uploads/[photoId]/Form.tsx
@@ -2,6 +2,8 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { Textarea } from '@nextui-org/input';
 import { Button } from '@nextui-org/button';
+import { Tooltip } from '@nextui-org/tooltip';
+import { Checkbox } from '@nextui-org/checkbox';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
@@ -9,6 +11,7 @@ import LocationInput from './LocationInput';
 import { useState } from 'react';
 import { Location } from '@/lib/types';
 import { profilePath } from '@/lib/constants';
+import { IconInfoCircle } from '@tabler/icons-react';
 
 type InputType = {
   caption: string;
@@ -49,19 +52,40 @@ export default function PhotoUploadForm({ photoId, disabled }: Props) {
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)} method="POST">
-        <div className="space-y-6 -mt-6">
+        <div className="-mt-6">
           <LocationInput setLocation={setLocation} />
           <Textarea
+            className="mt-6"
             label="Caption"
             variant="bordered"
             placeholder="(Optional)"
             {...register('caption')}
           />
+          <div className="flex gap-2 items-center">
+            <Checkbox className="mt-1" size="sm">
+              Allow for homepage display
+            </Checkbox>
+            <Tooltip
+              content={
+                <div className="px-1 py-2">
+                  <p className="text-small font-bold">Homepage Display</p>
+                  <p className="text-tiny mt-1">
+                    Allow your photo to be displayed on the homepage without a
+                    watermark. <br /> Proper measures will be taken to prevent
+                    unauthorized use of your photo.
+                  </p>
+                </div>
+              }
+              placement="top"
+            >
+              <IconInfoCircle size="1rem" className="mt-[15px] text-gray-400" />
+            </Tooltip>
+          </div>
           <Button
             color="primary"
             type="submit"
             isLoading={loading}
-            className="w-full"
+            className="w-full mt-6"
             disabled={disabled}
           >
             Save
