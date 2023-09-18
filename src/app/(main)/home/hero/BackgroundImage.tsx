801aaeec1dc983bf819e3cccfcd2c8bdffd2a69f
Ntholi Nkhatho <ntholi@live.com>
2023-09-18 15:31:11 +0200

    Don't use  next/image in hero

--- src/app/(main)/home/hero/BackgroundImage.tsx
+++ src/app/(main)/home/hero/BackgroundImage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 import { CurrentSlideData, PhotoWithData } from '@/lib/types';
 import MotionImage from './MotionImage';
 
--- src/app/(main)/home/hero/MotionImage.tsx
+++ src/app/(main)/home/hero/MotionImage.tsx
@@ -1,10 +1,9 @@
 import React, { ForwardedRef, forwardRef } from 'react';
-import Image from 'next/image';
 import { motion } from 'framer-motion';
 
 const ImageRef = forwardRef(
   (props: any, ref: ForwardedRef<HTMLImageElement>) => (
-    <Image {...props} ref={ref} />
+    <img {...props} ref={ref} />
   ),
 );
 ImageRef.displayName = 'Image';
