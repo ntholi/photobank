c69e79a1ec90a6456eefed17d48e7d01776f095e
Ntholi Nkhatho <ntholi@live.com>
2023-09-21 22:14:29 +0200

    create loading page for photo

--- src/app/(main)/photos/[id]/page.tsx
+++ src/app/(main)/photos/[id]/page.tsx
@@ -35,7 +35,7 @@ export default async function Page({ params }: Props) {
 
   return (
     <>
-      <div className="bg-gray-100 ">
+      <div className="bg-gray-100">
         <section className="container mx-auto px-4 md:px-20 py-10">
           <div className="grid grid-cols-1 md:grid-cols-5 gap-10">
             <div className="md:col-span-2 lg:col-span-3">
