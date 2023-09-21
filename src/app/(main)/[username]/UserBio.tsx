2658c9d8dc27de58cded4d82ac5f30a9fd9de03d
Ntholi Nkhatho <ntholi@live.com>
2023-09-21 08:58:08 +0200

    Show check marks for contributors

--- src/app/(main)/[username]/UserBio.tsx
+++ src/app/(main)/[username]/UserBio.tsx
@@ -1,13 +1,13 @@
 'use client';
 import { profilePath } from '@/lib/constants';
 import { Button } from '@nextui-org/button';
-import { Chip } from '@nextui-org/chip';
 import { Image } from '@nextui-org/image';
-import { User } from '@prisma/client';
+import { Role, User } from '@prisma/client';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { BiSolidBadgeCheck } from 'react-icons/bi';
 import WebsiteLink from './WebsiteLink';
+import { Popover, PopoverTrigger, PopoverContent } from '@nextui-org/popover';
 
 type Props = {
   user: User;
@@ -29,9 +29,33 @@ export default function UserBio({ user }: Props) {
         />
         <div className="ml-5 sm:ml-16 mt-2 sm:mt-3">
           <div className="flex flex-col md:flex-row gap-4 items-baseline">
-            <h1 className="text-xl sm:text-2xl font-bold text-zinc-600">
-              {`${user.firstName} ${user.lastName}`}
-            </h1>
+            <div className="flex items-center gap-2">
+              <h1 className="text-xl sm:text-2xl font-bold text-zinc-600">
+                {`${user.firstName} ${user.lastName}`}
+              </h1>
+              {hasCheckMark(user) && (
+                <Popover placement="right">
+                  <PopoverTrigger>
+                    <span
+                      color="primary"
+                      className={`cursor-pointer ${
+                        user.role === 'contributor'
+                          ? 'text-blue-500'
+                          : 'text-yellow-500'
+                      }`}
+                    >
+                      <BiSolidBadgeCheck size="1.2rem" />
+                    </span>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <div className="px-1 py-2 text-tiny">
+                      {checkMarkDescription(user.role)}
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              )}
+            </div>
+
             {session?.user?.id === user.id && (
               <Button
                 size="sm"
@@ -44,17 +68,6 @@ export default function UserBio({ user }: Props) {
               </Button>
             )}
           </div>
-          {user.role !== 'user' && (
-            <Chip
-              color="primary"
-              variant="bordered"
-              size="sm"
-              avatar={<BiSolidBadgeCheck size={8} />}
-              className="capitalize text-xs border-1"
-            >
-              {user?.role}
-            </Chip>
-          )}
           <div className="hidden md:block w-48 md:w-96 text-sm mt-3 text-foreground-500">
             <p>{user.bio}</p>
             <WebsiteLink user={user} />
@@ -67,6 +80,24 @@ export default function UserBio({ user }: Props) {
       </div>
     </>
   );
+}
+
+function checkMarkDescription(role: Role) {
+  if (role === 'admin') {
+    return 'User is an Admin';
+  } else if (role == 'moderator') {
+    return 'User can moderate content on this platform';
+  } else if (role == 'contributor') {
+    return 'User is a Contributor';
+  }
+}
+
+function hasCheckMark(user: User) {
+  const checkedRoles = ['admin', 'moderator', 'contributor'];
+  if (checkedRoles.includes(user.role)) {
+    return true;
+  }
+  return false;
 }
 
 const largeSize = (photoURL?: string | null) => {
