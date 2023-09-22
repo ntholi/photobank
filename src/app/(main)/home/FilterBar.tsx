9ae535e457a8dbc6f31d5f25f368199e756f3304
Ntholi Nkhatho <ntholi@live.com>
2023-09-22 20:34:00 +0200

    make FilterBar responsive

--- src/app/(main)/home/FilterBar.tsx
+++ src/app/(main)/home/FilterBar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { Tag } from '@prisma/client';
 import { Chip } from '@nextui-org/chip';
+import { ScrollShadow } from '@nextui-org/scroll-shadow';
 
 type Props = {
   setSelected: React.Dispatch<React.SetStateAction<Tag | null>>;
@@ -19,21 +20,25 @@ export default function FilterBar({ selected, setSelected }: Props) {
     });
   }, []);
   return (
-    <nav className="flex mt-2 gap-x-2">
-      {items.map((item) => (
-        <Chip
-          key={`${item.name}+${item.id}`}
-          className={'px-4 py-4 cursor-pointer'}
-          color={selected?.id === item.id ? 'primary' : 'default'}
-          onClick={() => {
-            console.log({ item });
-            console.log({ selected });
-            setSelected(selected?.id === item.id ? null : item);
-          }}
-        >
-          {item.name}
-        </Chip>
-      ))}
+    <nav>
+      <ScrollShadow orientation="horizontal" hideScrollBar offset={10}>
+        <div className="flex mt-2 gap-x-2">
+          {items.map((item) => (
+            <Chip
+              key={`${item.name}+${item.id}`}
+              className={'px-4 py-4 cursor-pointer'}
+              color={selected?.id === item.id ? 'primary' : 'default'}
+              onClick={() => {
+                console.log({ item });
+                console.log({ selected });
+                setSelected(selected?.id === item.id ? null : item);
+              }}
+            >
+              {item.name}
+            </Chip>
+          ))}
+        </div>
+      </ScrollShadow>
     </nav>
   );
 }
--- src/app/(main)/home/hero/Hero.tsx
+++ src/app/(main)/home/hero/Hero.tsx
@@ -21,7 +21,7 @@ export default function Home({ sliderData }: { sliderData: PhotoWithData[] }) {
     });
 
   return (
-    <main
+    <section
       className={`
         relative min-h-screen select-none overflow-hidden text-white antialiased`}
     >
@@ -30,15 +30,15 @@ export default function Home({ sliderData }: { sliderData: PhotoWithData[] }) {
           transitionData={transitionData}
           currentSlideData={currentSlideData}
         />
-        <div className="  absolute z-20  h-full w-full">
-          <div className=" flex h-full w-full grid-cols-10 flex-col md:grid">
-            <div className=" col-span-4 mb-3 flex h-full flex-1 flex-col justify-end px-5 md:mb-0 md:justify-center md:px-10">
+        <div className="absolute z-20  h-full w-full">
+          <div className="flex h-full w-full grid-cols-10 flex-col md:grid">
+            <div className="col-span-4 mb-3 flex h-full flex-1 flex-col justify-end px-5 md:mb-0 md:justify-center md:px-10">
               <SlideInfo
                 transitionData={transitionData}
                 currentSlideData={currentSlideData}
               />
             </div>
-            <div className=" col-span-6 flex h-full flex-1 flex-col justify-start p-4 md:justify-center md:p-10">
+            <div className="col-span-6 flex h-full flex-1 flex-col justify-start p-4 md:justify-center md:p-10">
               <Slides data={data} />
               <Controls
                 currentSlideData={currentSlideData}
@@ -54,6 +54,6 @@ export default function Home({ sliderData }: { sliderData: PhotoWithData[] }) {
           </div>
         </div>
       </AnimatePresence>
-    </main>
+    </section>
   );
 }
