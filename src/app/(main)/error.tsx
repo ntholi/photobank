4ad53579bc39dfdd4941bf47f7639a78a9993047
Ntholi Nkhatho <ntholi@live.com>
2023-09-26 12:00:03 +0200

    create error page

---
+++ src/app/(main)/error.tsx
@@ -0,0 +1,42 @@
+'use client'; // Error components must be Client Components
+
+import { useEffect } from 'react';
+import { Button } from '@nextui-org/button';
+import Image from 'next/image';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const logoHeight = 32;
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex h-screen flex-col justify-center items-center capitalize">
+      <Image
+        src={`/images/logo.jpg`}
+        width={logoHeight * 3.22}
+        height={logoHeight}
+        alt="logo"
+      />
+      <h2 className="text-3xl font-semibold mt-8">Something went wrong!</h2>
+      <Button
+        className="mt-5"
+        radius="none"
+        color="primary"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </Button>
+    </div>
+  );
+}
