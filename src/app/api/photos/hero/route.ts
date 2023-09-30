6488add49853b7b45da3bb3029211fffd62d37c8
Ntholi Nkhatho <ntholi@live.com>
2023-09-30 16:51:59 +0200

    use photos with useWithoutWatermark set to true

--- src/app/api/photos/hero/route.ts
+++ src/app/api/photos/hero/route.ts
@@ -1,34 +1,26 @@
+import { thumbnail } from '@/lib/config/urls';
 import { prisma } from '@/lib/db';
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
-    const photos = [
-        {
-            id: 2,
-            url: `https://d3tw6tl1trq98w.cloudfront.net/02.jpg`,
-            title: 'One',
+    const data = await prisma.photo.findMany({
+        where: {
+            status: 'published',
+            useWithoutWatermark: true,
         },
-        {
-            id: 1,
-            url: `https://d3tw6tl1trq98w.cloudfront.net/01.jpg`,
-            title: 'One',
+        include: {
+            user: true,
+            location: true,
         },
-        {
-            id: 3,
-            url: `https://d3tw6tl1trq98w.cloudfront.net/03.jpg`,
-            title: 'One',
-        },
-        {
-            id: 4,
-            url: `https://d3tw6tl1trq98w.cloudfront.net/04.jpg`,
-            title: 'One',
-        },
-        {
-            id: 6,
-            url: `https://d3tw6tl1trq98w.cloudfront.net/05.jpg`,
-            title: 'One',
-        },
-    ];
+    });
+
+    const photos = data.map((photo) => {
+        const fileName = photo.fileName.split('.')[0];
+        return {
+            ...photo,
+            url: thumbnail(fileName),
+        };
+    });
 
     return NextResponse.json({ photos });
 }
