fdd5457669229777966c5065543ba44f8bf0a91b
Ntholi Nkhatho <ntholi@live.com>
2023-10-25 15:58:57 +0200

    remove firebase files

--- src/app/api/users/userService.ts
+++ src/app/api/users/userService.ts
@@ -1,65 +1,65 @@
-import { prisma } from '@/lib/db';
+import prisma from '@/lib/db';
 import commonUrlPatterns from './commonUrlPatterns';
 import { User as FirebaseUser } from 'firebase/auth';
 import { UserRecord } from 'firebase-admin/lib/auth/user-record';
 
 export const saveUserToDB = async (firebaseUser: FirebaseUser | UserRecord) => {
-    const { firstName, lastName } = destructureNames(firebaseUser.displayName);
-    return await prisma.user.upsert({
-        where: {
-            id: firebaseUser.uid,
-        },
-        update: {},
-        create: {
-            id: firebaseUser.uid,
-            username: await generateUsername(firstName, lastName),
-            email: firebaseUser.email,
-            firstName: firstName,
-            lastName: lastName,
-            image: firebaseUser.photoURL,
-        },
-    });
+  const { firstName, lastName } = destructureNames(firebaseUser.displayName);
+  return await prisma.user.upsert({
+    where: {
+      id: firebaseUser.uid,
+    },
+    update: {},
+    create: {
+      id: firebaseUser.uid,
+      username: await generateUsername(firstName, lastName),
+      email: firebaseUser.email,
+      firstName: firstName,
+      lastName: lastName,
+      image: firebaseUser.photoURL,
+    },
+  });
 };
 
 export async function generateUsername(firstName: string, lastName: string) {
-    let username = firstName.toLowerCase();
-    let userWithSameUsername = await prisma.user.findUnique({
-        where: {
-            username: username,
-        },
+  let username = firstName.toLowerCase();
+  let userWithSameUsername = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+
+  let counter = 2;
+  while (userWithSameUsername) {
+    username = `${firstName.toLowerCase()}${lastName.toLowerCase()}`;
+
+    if (counter > 2) {
+      username += counter;
+    }
+    userWithSameUsername = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
     });
+    counter++;
+  }
 
-    let counter = 2;
-    while (userWithSameUsername) {
-        username = `${firstName.toLowerCase()}${lastName.toLowerCase()}`;
+  if (commonUrlPatterns.includes(username) || username.length < 3) {
+    username += 'user'; // Append 'user' to the end
+  }
 
-        if (counter > 2) {
-            username += counter;
-        }
-        userWithSameUsername = await prisma.user.findUnique({
-            where: {
-                username: username,
-            },
-        });
-        counter++;
-    }
-
-    if (commonUrlPatterns.includes(username)) {
-        username += 'user'; // Append 'user' to the end
-    }
-
-    return username;
+  return username;
 }
 
 export function destructureNames(names?: string | null) {
-    if (!names) return { firstName: '', lastName: '' };
+  if (!names) return { firstName: '', lastName: '' };
 
-    let firstName = names;
-    let lastName = '';
-    if (names && names.split(' ').length >= 2) {
-        const namesArray = names.split(' ');
-        firstName = namesArray.slice(0, namesArray.length - 1).join(' ');
-        lastName = namesArray[namesArray.length - 1];
-    }
-    return { firstName, lastName };
+  let firstName = names;
+  let lastName = '';
+  if (names && names.split(' ').length >= 2) {
+    const namesArray = names.split(' ');
+    firstName = namesArray.slice(0, namesArray.length - 1).join(' ');
+    lastName = namesArray[namesArray.length - 1];
+  }
+  return { firstName, lastName };
 }
