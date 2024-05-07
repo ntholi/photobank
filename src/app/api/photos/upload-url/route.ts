6688d8b4bea447d26be45bd7a018c0713f67f955
Ntholi Nkhatho <ntholi@live.com>
2024-05-07 11:40:37 +0200

    chore: Add permission check for photo upload

--- src/app/api/photos/upload-url/route.ts
+++ src/app/api/photos/upload-url/route.ts
@@ -5,24 +5,32 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { nanoid } from 'nanoid';
 import { bucketName, s3Client } from '@/lib/config/aws';
 import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { canUpload } from '@/lib/helpers/user-helper';
 
 export async function GET(req: Request) {
-    const session = await getServerSession(authOptions);
-    if (!session?.user) {
-        return NextResponse.json(
-            { error: 'You must be logged in to upload a photo' },
-            { status: 401 },
-        );
-    }
-    const { searchParams } = new URL(req.url);
-    const ext = searchParams.get('ext') || '';
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    return NextResponse.json(
+      { error: 'You must be logged in to upload a photo' },
+      { status: 401 },
+    );
+  }
+  if (!canUpload(session.user)) {
+    return NextResponse.json(
+      { error: 'You do not have permission to upload a photo' },
+      { status: 403 },
+    );
+  }
 
-    const fileName = `${nanoid()}.${ext}`;
-    const command = new PutObjectCommand({
-        Bucket: bucketName,
-        Key: fileName,
-    });
-    const url = await getSignedUrl(s3Client, command, { expiresIn: 60 * 60 });
+  const { searchParams } = new URL(req.url);
+  const ext = searchParams.get('ext') || '';
 
-    return NextResponse.json({ url, fileName });
+  const fileName = `${nanoid()}.${ext}`;
+  const command = new PutObjectCommand({
+    Bucket: bucketName,
+    Key: fileName,
+  });
+  const url = await getSignedUrl(s3Client, command, { expiresIn: 60 * 60 });
+
+  return NextResponse.json({ url, fileName });
 }
