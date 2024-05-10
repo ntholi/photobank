967dc094424f62ce0a252359005058f79c64bec9
Ntholi Nkhatho <ntholi@live.com>
2024-05-10 16:39:21 +0200

    chore: Update CategoryPage component to fetch and display category details

--- src/app/(admin)/admin/categories/[id]/page.tsx
+++ src/app/(admin)/admin/categories/[id]/page.tsx
@@ -1,3 +1,6 @@
+import { DetailsView, FieldView } from '@/app/(admin)/admin-core';
+import prisma from '@/lib/db';
+import { notFound } from 'next/navigation';
 import React from 'react';
 
 type Props = {
@@ -5,6 +8,20 @@ type Props = {
     id: string;
   };
 };
-export default function CategoryPage({ params: { id } }: Props) {
-  return <div>{id}</div>;
+export default async function CategoryPage({ params: { id } }: Props) {
+  const data = await prisma.category.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  if (!data) {
+    return notFound();
+  }
+
+  return (
+    <DetailsView>
+      <FieldView label="Name" value={data.name} />
+    </DetailsView>
+  );
 }
