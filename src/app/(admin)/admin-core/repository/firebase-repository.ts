a3cf3aa6199de4d07812e3cd21dbbca1d0fba89c
Ntholi Nkhatho <ntholi@live.com>
2024-05-11 09:40:15 +0200

    chore: Refactor FirebaseRepository to use Firestore instance and improve code readability

--- src/app/(admin)/admin-core/repository/firebase-repository.ts
+++ src/app/(admin)/admin-core/repository/firebase-repository.ts
@@ -13,18 +13,22 @@ import {
   where,
   limit as limitQuery,
   Query,
+  Firestore,
 } from 'firebase/firestore';
-import { db } from '@/lib/config/firebase';
 import { Repository, Resource, ResourceCreate } from './repository';
 
 export class FirebaseRepository<T extends Resource> implements Repository<T> {
-  constructor(protected readonly collectionName: string) {}
+  constructor(
+    readonly db: Firestore,
+
+    protected readonly collectionName: string,
+  ) {}
 
   listen(
     callback: (resources: T[]) => void,
     filter?: { field: string; value: any },
   ): () => void {
-    const ref = collection(db, this.collectionName);
+    const ref = collection(this.db, this.collectionName);
     const q = filter
       ? query(ref, where(filter.field, '==', filter.value))
       : ref;
@@ -39,7 +43,7 @@ export class FirebaseRepository<T extends Resource> implements Repository<T> {
   }
 
   listenForDocument(id: string, callback: (resource: T) => void): () => void {
-    const docRef = doc(db, this.collectionName, id);
+    const docRef = doc(this.db, this.collectionName, id);
     const unsubscribe = onSnapshot(docRef, (docSnap) => {
       if (docSnap.exists()) {
         callback({ id: docSnap.id, ...docSnap.data() } as T);
@@ -52,7 +56,7 @@ export class FirebaseRepository<T extends Resource> implements Repository<T> {
     limit = 10,
     filter?: { field: string; value: any },
   ): Promise<T[]> {
-    const ref = collection(db, this.collectionName);
+    const ref = collection(this.db, this.collectionName);
     const q = filter
       ? query(ref, where(filter.field, '==', filter.value), limitQuery(limit))
       : query(ref, limitQuery(limit));
@@ -62,7 +66,7 @@ export class FirebaseRepository<T extends Resource> implements Repository<T> {
   }
 
   async get(id: string): Promise<T | undefined> {
-    const docRef = doc(db, this.collectionName, id);
+    const docRef = doc(this.db, this.collectionName, id);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
       return { id: docSnap.id, ...docSnap.data() } as T;
@@ -71,7 +75,7 @@ export class FirebaseRepository<T extends Resource> implements Repository<T> {
   }
 
   async create(resource: ResourceCreate<T>): Promise<T> {
-    const docRef = await addDoc(collection(db, this.collectionName), {
+    const docRef = await addDoc(collection(this.db, this.collectionName), {
       ...resource,
       createdAt: serverTimestamp(),
       updatedAt: serverTimestamp(),
@@ -80,7 +84,7 @@ export class FirebaseRepository<T extends Resource> implements Repository<T> {
   }
 
   async update(id: string, resource: Omit<T, 'id'>): Promise<T> {
-    const docRef = doc(db, this.collectionName, id);
+    const docRef = doc(this.db, this.collectionName, id);
     await setDoc(docRef, {
       ...resource,
       updatedAt: serverTimestamp(),
@@ -89,12 +93,12 @@ export class FirebaseRepository<T extends Resource> implements Repository<T> {
   }
 
   async delete(id: string): Promise<void> {
-    await deleteDoc(doc(db, this.collectionName, id));
+    await deleteDoc(doc(this.db, this.collectionName, id));
   }
 
   async getAllBy(field: string, value: string, limit = 8): Promise<T[]> {
     const q = query(
-      collection(db, this.collectionName),
+      collection(this.db, this.collectionName),
       where(field, '==', value),
       orderBy('createdAt', 'desc'),
       limitQuery(limit),
@@ -106,7 +110,7 @@ export class FirebaseRepository<T extends Resource> implements Repository<T> {
 
   async search(field: string, value: string): Promise<T[]> {
     const q = query(
-      collection(db, this.collectionName),
+      collection(this.db, this.collectionName),
       where(field, '>=', value),
       where(field, '<=', value + '\uf8ff'),
     );
@@ -119,7 +123,7 @@ export class FirebaseRepository<T extends Resource> implements Repository<T> {
   }
 
   docRef(id: string) {
-    return doc(db, this.collectionName, id);
+    return doc(this.db, this.collectionName, id);
   }
 
   async getDocs(query: Query): Promise<T[]> {
@@ -138,7 +142,7 @@ export class FirebaseRepository<T extends Resource> implements Repository<T> {
     resourceName: string,
     id: string,
   ): Promise<R> {
-    const docRef = doc(db, resourceName, id);
+    const docRef = doc(this.db, resourceName, id);
     const docSnap = await getDoc(docRef);
     return { id: docSnap.id, ...docSnap.data() } as R;
   }
@@ -146,7 +150,10 @@ export class FirebaseRepository<T extends Resource> implements Repository<T> {
   async getResourceList<R extends Resource>(
     resourceName: string,
   ): Promise<R[]> {
-    const q = query(collection(db, resourceName), orderBy('createdAt', 'desc'));
+    const q = query(
+      collection(this.db, resourceName),
+      orderBy('createdAt', 'desc'),
+    );
     const snapshot = await getDocs(q);
     if (snapshot.empty) return [];
     return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }) as R);
