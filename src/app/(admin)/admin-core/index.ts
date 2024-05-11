e8e3c15075a84645b193b000a13da5240a745507
Ntholi Nkhatho <ntholi@live.com>
2024-05-11 09:41:39 +0200

    remove unused components

--- src/app/(admin)/admin-core/index.ts
+++ src/app/(admin)/admin-core/index.ts
@@ -5,9 +5,7 @@ import CreateView, { CreateViewProps } from './CreateView';
 import FieldView from './field-views/FieldView';
 import ReferenceView from './field-views/ReferenceView';
 import ImagePicker from './form/ImagePicker';
-import TextField from './form/TextField';
 import ReferenceField from './form/ReferenceField';
-import SelectField from './form/SelectField';
 import { FirebaseRepository } from './repository/firebase-repository';
 
 export {
@@ -17,9 +15,7 @@ export {
   CreateView,
   ImagePicker,
   FieldView,
-  TextField,
   ReferenceField,
-  SelectField,
   ReferenceView,
   FirebaseRepository,
 };
