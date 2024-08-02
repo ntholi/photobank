95ecceb0d33de23e22b0308ebf56c14afbe6ce15
Ntholi Nkhatho <ntholi@live.com>
2024-08-02 18:20:39 +0200

    refactor: Remove unused description field in tag form

--- src/app/(admin)/admin/tags/actions.ts
+++ src/app/(admin)/admin/tags/actions.ts
@@ -1,16 +1,13 @@
 'use server';
 import { auth } from '@/auth';
 import prisma from '@/lib/prisma';
-import { Label, Tag } from '@prisma/client';
+import { Tag } from '@prisma/client';
 import { revalidatePath } from 'next/cache';
 
 export async function getTag(id: number) {
   return await prisma.tag.findUnique({
     where: {
       id: Number(id),
-    },
-    include: {
-      labels: true,
     },
   });
 }
@@ -22,54 +19,21 @@ export async function deleteTag(id: number) {
   revalidatePath('/admin/tags');
 }
 
-export async function createTag(tag: Tag & { labels: Label[] }) {
+export async function createTag(data: Tag) {
   const session = await auth();
   if (session?.user?.role != 'admin') throw new Error('User not admin');
 
-  const { labels, ...data } = tag;
   return await prisma.tag.create({
-    data: {
-      ...data,
-      labels: {
-        create: labels,
-      },
-    },
+    data,
   });
 }
 
-export async function updateTag(id: number, data: Tag & { labels: Label[] }) {
+export async function updateTag(id: number, data: Tag) {
   const session = await auth();
   if (session?.user?.role != 'admin') throw new Error('User not admin');
 
-  const { labels, ...tagData } = data;
-
-  // First, fetch the current labels
-  const currentTag = await prisma.tag.findUnique({
-    where: { id: Number(id) },
-    include: { labels: true },
-  });
-
-  if (!currentTag) {
-    throw new Error('Tag not found');
-  }
-
-  const currentLabelIds = new Set(currentTag.labels.map((l) => l.id));
-  const newLabelIds = new Set(labels.map((l) => l.id));
-
   return prisma.tag.update({
     where: { id: Number(id) },
-    data: {
-      ...tagData,
-      labels: {
-        disconnect: currentTag.labels
-          .filter((label) => !newLabelIds.has(label.id))
-          .map((label) => ({ id: label.id })),
-        connectOrCreate: labels.map((label) => ({
-          where: { id: label.id },
-          create: { name: label.name },
-        })),
-      },
-    },
-    include: { labels: true },
+    data,
   });
 }
