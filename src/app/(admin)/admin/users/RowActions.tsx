30254066ba590a09dffa218401038ba9af2387ea
Ntholi Nkhatho <ntholi@live.com>
2024-08-03 09:17:48 +0200

    refactor: Update RowActions component to allow updating user roles

--- src/app/(admin)/admin/users/RowActions.tsx
+++ src/app/(admin)/admin/users/RowActions.tsx
@@ -1,20 +1,35 @@
 'use client';
 
 import { ActionIcon, Menu, Text } from '@mantine/core';
-import { User } from '@prisma/client';
+import { Role, User } from '@prisma/client';
 import {
   IconDotsVertical,
   IconHandStop,
   IconLockOpen,
 } from '@tabler/icons-react';
 import React, { useEffect } from 'react';
-import { blockUser } from './actions';
+import { blockUser, updateRole } from './actions';
+import { modals } from '@mantine/modals';
 
 type Props = {
   user: User;
 };
+
 export default function RowActions({ user }: Props) {
   const [blocked, setBlocked] = React.useState(user.blocked);
+
+  const openModal = (actionName: string, onConfirm: () => void) =>
+    modals.openConfirmModal({
+      title: 'Please confirm your action',
+      children: (
+        <Text size="sm">
+          Are you sure you want to {actionName.toLowerCase()} this user?
+        </Text>
+      ),
+      labels: { confirm: 'Confirm', cancel: 'Cancel' },
+      onCancel: () => console.log('Cancel'),
+      onConfirm: onConfirm,
+    });
 
   useEffect(() => {
     setBlocked(user.blocked);
@@ -23,6 +38,10 @@ export default function RowActions({ user }: Props) {
   const handleBlock = async () => {
     const res = await blockUser(user.id, !blocked);
     if (res) setBlocked(!blocked);
+  };
+
+  const handleRole = async (role: Role) => {
+    openModal(`update role to ${role} for`, () => updateRole(user.id, role));
   };
 
   return (
@@ -42,6 +61,21 @@ export default function RowActions({ user }: Props) {
           onClick={handleBlock}
         >
           <Text size="xs">{blocked ? 'Unblock' : 'Block'} Account</Text>
+        </Menu.Item>
+        <Menu.Divider />
+
+        <Menu.Label>Update Role</Menu.Label>
+        <Menu.Item onClick={() => handleRole('user')}>
+          <Text size="xs">User</Text>
+        </Menu.Item>
+        <Menu.Item onClick={() => handleRole('contributor')}>
+          <Text size="xs">Contributor</Text>
+        </Menu.Item>
+        <Menu.Item onClick={() => handleRole('moderator')}>
+          <Text size="xs">Moderator</Text>
+        </Menu.Item>
+        <Menu.Item onClick={() => handleRole('admin')}>
+          <Text size="xs">Admin</Text>
         </Menu.Item>
       </Menu.Dropdown>
     </Menu>
--- src/app/(admin)/admin/users/actions.ts
+++ src/app/(admin)/admin/users/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import prisma from '@/lib/prisma';
+import { Role } from '@prisma/client';
 import { revalidatePath } from 'next/cache';
 
 export async function blockUser(id: string, blocked: boolean) {
@@ -10,4 +11,13 @@ export async function blockUser(id: string, blocked: boolean) {
   });
   revalidatePath('/admin/users');
   return res.blocked === blocked;
+}
+
+export async function updateRole(id: string, role: Role) {
+  const res = await prisma.user.update({
+    where: { id },
+    data: { role },
+  });
+  revalidatePath('/admin/users');
+  return res.role === role;
 }
