185af3088a0ad5b31a5fdb75bd26ca5a4a7926fe
Ntholi Nkhatho <ntholi@live.com>
2024-08-06 12:10:55 +0200

    refactor: Remove commented out code and add error handling in signup route

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -7,32 +7,6 @@ export async function POST(request: Request) {
   if (!email || !password) {
     throw new Error('Missing Fields');
   }
-  /*
-  try {
-    const user = await adminAuth.createUser({
-      email,
-      password,
-      displayName: names,
-    });*/
-  //await saveUserToDB(user);
-  //return NextResponse.json({ user });
-  /*
-  } catch (error: FirebaseError | any) {
-    console.log(error);
-    if (error.code === 'auth/email-already-exists') {
-      return NextResponse.json(
-        { error: 'Email already exists' },
-        { status: 400 },
-      );
-    } else if (error.code === 'auth/invalid-email') {
-      return NextResponse.json({ error: 'Invalid email' }, { status: 400 });
-    } else if (error.code === 'auth/weak-password') {
-      return NextResponse.json({ error: 'Weak password' }, { status: 400 });
-    } else {
-      return NextResponse.json(
-        { error: 'Something went wrong' },
-        { status: 500 },
-      );
-    }
-  }*/
+
+  throw new Error('Not implemented');
 }
