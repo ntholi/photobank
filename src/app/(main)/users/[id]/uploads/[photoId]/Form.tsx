ae493e293caea5128083827c7c1b1676d3e31d35
Ntholi Nkhatho <ntholi@live.com>
2024-08-21 21:12:49 +0200

    refactor: Remove unused code in PhotoUploadForm component

--- src/app/(main)/users/[id]/uploads/[photoId]/Form.tsx
+++ src/app/(main)/users/[id]/uploads/[photoId]/Form.tsx
@@ -27,7 +27,6 @@ export default function PhotoUploadForm({ photoId, disabled }: Props) {
   const { user } = useSession().data || {};
   const router = useRouter();
   const [location, setLocation] = useState<InputType['location'] | null>(null);
-  const [useWithoutWatermark, setUseWithoutWatermark] = useState(true);
 
   const onSubmit: SubmitHandler<InputType> = async (data) => {
     setLoading(true);
@@ -36,7 +35,6 @@ export default function PhotoUploadForm({ photoId, disabled }: Props) {
       if (location) {
         data.location = location;
       }
-      data.useWithoutWatermark = useWithoutWatermark;
       await axios.put(`/api/photos/${photoId}`, data);
       router.push(profilePath(user));
     } catch (error) {
@@ -57,31 +55,6 @@ export default function PhotoUploadForm({ photoId, disabled }: Props) {
           placeholder='(Optional)'
           {...register('caption')}
         />
-        <div className='flex items-center gap-2'>
-          <Checkbox
-            className='mt-1'
-            size='sm'
-            checked={useWithoutWatermark}
-            onValueChange={setUseWithoutWatermark}
-          >
-            Allow for homepage display
-          </Checkbox>
-          <Tooltip
-            content={
-              <div className='px-1 py-2'>
-                <p className='text-small font-bold'>Homepage Display</p>
-                <p className='mt-1 text-tiny'>
-                  Allow your photo to be displayed on the homepage without a
-                  watermark. <br /> Proper measures will be taken to prevent
-                  unauthorized use of your photo.
-                </p>
-              </div>
-            }
-            placement='top'
-          >
-            <IconInfoCircle size='1rem' className='mt-[15px] text-gray-400' />
-          </Tooltip>
-        </div>
         <Button
           color='primary'
           type='submit'
