ffd3e67335102a5922393c4a8f0e9065e1376cf3
Ntholi Nkhatho <ntholi@live.com>
2024-11-03 19:16:49 +0200

    add detectLabels

--- src/server/photos/imageService.ts
+++ src/server/photos/imageService.ts
@@ -1,16 +1,38 @@
 import { s3Client, bucketName, processedBucketName } from '@/lib/config/aws';
 import { GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
+import {
+  RekognitionClient,
+  DetectLabelsCommand,
+} from '@aws-sdk/client-rekognition';
 import Sharp from 'sharp';
 import WatermarkUtility from './WatermarkUtility';
+
+const rekognitionClient = new RekognitionClient({
+  region: process.env.AWS_BUCKET_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_BUCKET_ACCESS_KEY || '',
+    secretAccessKey: process.env.AWS_BUCKET_SECRET_KEY || '',
+  },
+});
 
 interface ProcessedImages {
   thumbnail: string;
   watermarked: string;
+  labels: (Label | undefined)[];
+}
+
+interface Label {
+  name: string | undefined;
+  confidence: number | undefined;
 }
 
 export async function processImage(fileName: string): Promise<ProcessedImages> {
   try {
-    // Get the original image from S3
+    // Validate input
+    if (!fileName) {
+      throw new Error('fileName is required');
+    }
+
     const getCommand = new GetObjectCommand({
       Bucket: bucketName,
       Key: fileName,
@@ -57,9 +79,14 @@ export async function processImage(fileName: string): Promise<ProcessedImages> {
       ),
     ]);
 
+    // Wait a short time to ensure S3 consistency
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const labels = await detectLabels(thumbnailKey);
     return {
       thumbnail: thumbnailKey,
       watermarked: watermarkedKey,
+      labels: labels,
     };
   } catch (error) {
     console.error('Error processing image:', error);
@@ -67,8 +94,49 @@ export async function processImage(fileName: string): Promise<ProcessedImages> {
   }
 }
 
-// Helper function to convert stream to buffer
+async function detectLabels(key: string): Promise<(Label | undefined)[]> {
+  try {
+    const command = new DetectLabelsCommand({
+      Image: {
+        S3Object: {
+          Bucket: processedBucketName,
+          Name: key,
+        },
+      },
+      MaxLabels: 20,
+      MinConfidence: 85,
+    });
+
+    const labelResponse = await rekognitionClient.send(command);
+    const labels = labelResponse?.Labels?.map((label) => {
+      return {
+        name: label.Name,
+        confidence: label.Confidence,
+        categories: label.Categories?.map((category) => category.Name),
+      };
+    }).flatMap((it) =>
+      it.categories?.map((category) => ({
+        name: category,
+        confidence: it.confidence,
+      })),
+    );
+
+    return labels ?? [];
+  } catch (error) {
+    console.error('Error in detectLabels:', {
+      error,
+      key,
+      bucket: processedBucketName,
+    });
+    throw error;
+  }
+}
+
 async function streamToBuffer(stream: any): Promise<Buffer> {
+  if (!stream) {
+    throw new Error('Stream is required');
+  }
+
   const chunks: Buffer[] = [];
 
   return new Promise((resolve, reject) => {
