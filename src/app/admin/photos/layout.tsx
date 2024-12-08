ff0333f244e77dc741527fe3d5d90e9f5f85b3c2
Ntholi Nkhatho <ntholi@live.com>
2024-12-08 08:37:33 +0200

    Enhance photo layout component with image previews and improved item labels
    - Added image thumbnails to the photo list items using Mantine's Image component.
    - Updated item labels to display a shortened description or file name for better clarity.
    - Cleaned up imports for better organization.

--- src/app/admin/photos/layout.tsx
+++ src/app/admin/photos/layout.tsx
@@ -3,6 +3,9 @@
 import { PropsWithChildren } from 'react';
 import { ListItem, ListLayout, NewLink } from '@/components/adease';
 import { getAllPhotos } from './actions';
+import { shorten } from '@/utils';
+import { thumbnail } from '@/lib/config/urls';
+import { Image } from '@mantine/core';
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
@@ -11,7 +14,21 @@ export default function Layout({ children }: PropsWithChildren) {
       queryKey={['photos']}
       getItems={getAllPhotos}
       actionIcons={[<NewLink key={'new-link'} href='/admin/photos/new' />]}
-      renderItem={(it) => <ListItem id={it.id} label={it.id} />}
+      renderItem={(it) => (
+        <ListItem
+          id={it.id}
+          label={shorten(it.description) || it.fileName}
+          leftSection={
+            <Image
+              src={thumbnail(it.fileName)}
+              alt={it.fileName}
+              w={40}
+              h={40}
+              radius='sm'
+            />
+          }
+        />
+      )}
     >
       {children}
     </ListLayout>
