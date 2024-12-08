45038849b386be8b0d681e36ea5dcb41771af92c
Ntholi Nkhatho <ntholi@live.com>
2024-12-08 07:48:04 +0200

    Refactor UserDetails component to enhance layout using Mantine's Grid and Card components. Improved organization of user information by separating details into two cards for better readability.

--- src/app/admin/users/[id]/page.tsx
+++ src/app/admin/users/[id]/page.tsx
@@ -6,6 +6,7 @@ import {
 } from '@/components/adease';
 import { notFound } from 'next/navigation';
 import { getUser, deleteUser } from '../actions';
+import { Card, CardSection, Grid, GridCol, Stack, Text } from '@mantine/core';
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -14,28 +15,42 @@ type Props = {
 export default async function UserDetails({ params }: Props) {
   const { id } = await params;
   const users = await getUser(id);
-  
+
   if (!users) {
     return notFound();
   }
 
   return (
     <DetailsView>
-      <DetailsViewHeader 
-        title={'User'} 
+      <DetailsViewHeader
+        title={'User'}
         queryKey={['users']}
         handleDelete={async () => {
           'use server';
           await deleteUser(id);
         }}
       />
-      <DetailsViewBody>
-        <FieldView label='Name'>{users.name}</FieldView>
-        <FieldView label='Email'>{users.email}</FieldView>
-        <FieldView label='Role'>{users.role}</FieldView>
-        <FieldView label='Blocked'>{users.blocked}</FieldView>
-        <FieldView label='Website'>{users.website}</FieldView>
-        <FieldView label='Bio'>{users.bio}</FieldView>
+      <DetailsViewBody p={'xs'}>
+        <Grid>
+          <GridCol span={{ base: 12, md: 6 }}>
+            <Card withBorder>
+              <Stack gap={'sm'}>
+                <FieldView label='Name'>{users.name}</FieldView>
+                <FieldView label='Email'>{users.email}</FieldView>
+                <FieldView label='Website'>{users.website}</FieldView>
+                <FieldView label='Bio'>{users.bio}</FieldView>
+              </Stack>
+            </Card>
+          </GridCol>
+          <GridCol span={{ base: 12, md: 6 }}>
+            <Card withBorder>
+              <Stack gap={'sm'}>
+                <FieldView label='Role'>{users.role}</FieldView>
+                <FieldView label='Status'>{users.status}</FieldView>
+              </Stack>
+            </Card>
+          </GridCol>
+        </Grid>
       </DetailsViewBody>
     </DetailsView>
   );
