960ff736f80593d512cdcb6ab5234a516228e767
Ntholi Nkhatho <ntholi@live.com>
2024-12-08 06:39:42 +0200

    Add metadata for Admin layout component

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,8 +5,17 @@ import '@mantine/notifications/styles.css';
 import Dashboard from './dashboard';
 import Providers from './providers';
 import React from 'react';
+import { Metadata } from 'next';
 
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
+export const metadata: Metadata = {
+  title: 'Admin',
+};
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <Providers>
       <Dashboard>{children}</Dashboard>
