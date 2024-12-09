840149482bf6ad349b148a4a9df2230a76cef407
Ntholi Nkhatho <ntholi@live.com>
2024-12-09 20:32:01 +0200

    Refactor page component to utilize Suspense for content loading
    - Added Suspense and Loader components from Mantine for improved user experience during content fetching.
    - Moved content fetching logic into a new asynchronous Content function to streamline the page component.
    - Updated the rendering of ContentForm to be wrapped in Suspense, providing a fallback loader while data is being fetched.

--- src/app/admin/content/[slug]/page.tsx
+++ src/app/admin/content/[slug]/page.tsx
@@ -1,6 +1,7 @@
-import { Box } from '@mantine/core';
+import { Box, Center, Loader, Skeleton } from '@mantine/core';
 import { getContent } from '../actions';
 import ContentForm from './ContentForm';
+import { Suspense } from 'react';
 
 type Props = {
   params: Promise<{
@@ -9,11 +10,23 @@ type Props = {
 };
 
 export default async function page({ params }: Props) {
-  const { slug } = await params;
-  const content = await getContent(slug);
   return (
     <Box p={'md'}>
-      <ContentForm slug={slug} content={content} />
+      <Suspense
+        fallback={
+          <Center mt={100}>
+            <Loader />
+          </Center>
+        }
+      >
+        <Content params={params} />
+      </Suspense>
     </Box>
   );
+}
+
+async function Content({ params }: Props) {
+  const { slug } = await params;
+  const content = await getContent(slug);
+  return <ContentForm slug={slug} content={content} />;
 }
