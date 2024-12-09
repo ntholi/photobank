374d93c728ce2b9e4231ea027d2e9802a255e006
Ntholi Nkhatho <ntholi@live.com>
2024-12-09 15:11:26 +0200

    Enhance contributor applications layout to display application status with icons
    - Added a Status component to visually represent the status of contributor applications using icons for approved, rejected, and pending states.
    - Updated ListItem to include the new Status component in the contributor applications layout.

--- src/app/admin/contributor-applications/layout.tsx
+++ src/app/admin/contributor-applications/layout.tsx
@@ -3,6 +3,9 @@
 import { PropsWithChildren } from 'react';
 import { ListItem, ListLayout, NewLink } from '@/components/adease';
 import { getAllContributorApplications } from './actions';
+import { IconBan, IconHourglass } from '@tabler/icons-react';
+import { IconCheck } from '@tabler/icons-react';
+import { ApplicationStatus } from '@prisma/client';
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
@@ -14,10 +17,25 @@ export default function Layout({ children }: PropsWithChildren) {
         <NewLink key={'new-link'} href='/admin/contributor-applications/new' />,
       ]}
       renderItem={(it) => (
-        <ListItem id={it.id} label={it.user.name || it.user.id} />
+        <ListItem
+          id={it.id}
+          label={it.user.name || it.user.id}
+          rightSection={<Status status={it.status} />}
+        />
       )}
     >
       {children}
     </ListLayout>
   );
+}
+
+function Status({ status }: { status: ApplicationStatus }) {
+  switch (status) {
+    case 'approved':
+      return <IconCheck size={'1rem'} color='green' />;
+    case 'rejected':
+      return <IconBan size={'1rem'} color='red' />;
+    default:
+      return <IconHourglass size={'1rem'} color='gray' />;
+  }
 }
