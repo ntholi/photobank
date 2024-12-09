18c0c47a7a129babc430f4200017c3c3020ee2a4
Ntholi Nkhatho <ntholi@live.com>
2024-12-09 22:15:27 +0200

    Refactor GET function in filters route to remove unused parameters for cleaner code

--- src/app/api/filters/route.ts
+++ src/app/api/filters/route.ts
@@ -1,9 +1,7 @@
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
-export async function GET(req: Request, res: Response) {
-  const { searchParams } = new URL(req.url || '');
-
+export async function GET() {
   const filters = await prisma.tag.findMany({
     take: 10,
     //TODO: TAKE MOST USED TAGS
