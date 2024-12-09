ccaa80bd4f9779113edfb39d93147eaa5bb094ad
Ntholi Nkhatho <ntholi@live.com>
2024-12-09 22:09:46 +0200

    Refactor ContentPage component to await params and destructure slug for improved readability

--- src/app/(main)/content/[slug]/page.tsx
+++ src/app/(main)/content/[slug]/page.tsx
@@ -4,9 +4,9 @@ import Container from '../../base/Container';
 import { Content } from '@prisma/client';
 
 type Props = {
-  params: {
+  params: Promise<{
     slug: string;
-  };
+  }>;
 };
 
 async function getContent(slug: string): Promise<Content | null> {
@@ -16,7 +16,8 @@ async function getContent(slug: string): Promise<Content | null> {
 }
 
 export default async function ContentPage({ params }: Props) {
-  const content = await getContent(params.slug);
+  const { slug } = await params;
+  const content = await getContent(slug);
 
   if (!content) {
     return notFound();
@@ -25,7 +26,7 @@ export default async function ContentPage({ params }: Props) {
   return (
     <Container className='py-16'>
       <article className='prose lg:prose-lg mx-auto max-w-3xl'>
-        <h1 className='mb-2 text-3xl font-bold'>{slugToTitle(params.slug)}</h1>
+        <h1 className='mb-2 text-3xl font-bold'>{slugToTitle(slug)}</h1>
         <div dangerouslySetInnerHTML={{ __html: content.body || '' }} />
       </article>
     </Container>
