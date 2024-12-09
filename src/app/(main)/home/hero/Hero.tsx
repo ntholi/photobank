8b8f48ce5f13d57f19522c268eabe275aff6e679
Ntholi Nkhatho <ntholi@live.com>
2024-12-09 12:09:25 +0200

    Fix "Encountered two children with the same key" issue

--- src/app/(main)/home/hero/Hero.tsx
+++ src/app/(main)/home/hero/Hero.tsx
@@ -57,10 +57,11 @@ export default function Hero({ sliderData }: { sliderData: PhotoWithData[] }) {
     >
       <AnimatePresence>
         <BackgroundImage
+          key="background"
           transitionData={transitionData}
           currentSlideData={currentSlideData}
         />
-        <div className="absolute z-20  h-full w-full">
+        <div key="content" className="absolute z-20  h-full w-full">
           <div className="flex h-full w-full grid-cols-10 flex-col md:grid">
             <div className="col-span-4 mb-3 flex h-full flex-1 flex-col justify-end px-5 md:mb-0 md:justify-center md:px-10">
               <SlideInfo
