39c4c3a1ab985535cfd736c64bfb87be720643ad
Ntholi Nkhatho <ntholi@live.com>
2025-08-16 09:40:39 +0200

    Refactor database schema: update user, account, session, and location tables for consistency, remove unnecessary parameters, and add unique constraints for credential IDs and place IDs.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -14,32 +14,32 @@ export const userRoles = ['user', 'contributor', 'moderator', 'admin'] as const;
 export type UserRole = (typeof userRoles)[number];
 
 export const users = pgTable('users', {
-  id: text('id')
+  id: text()
     .primaryKey()
     .$defaultFn(() => nanoid()),
-  name: text('name'),
-  role: text('role', { enum: userRoles }).notNull().default('user'),
-  email: text('email').unique(),
-  emailVerified: timestamp('email_verified', { mode: 'date' }),
-  image: text('image'),
+  name: text(),
+  role: text({ enum: userRoles }).notNull().default('user'),
+  email: text().unique(),
+  emailVerified: timestamp({ mode: 'date' }),
+  image: text(),
 });
 
 export const accounts = pgTable(
   'accounts',
   {
-    userId: text('user_id')
+    userId: text()
       .notNull()
       .references(() => users.id, { onDelete: 'cascade' }),
-    type: text('type').$type<AdapterAccountType>().notNull(),
-    provider: text('provider').notNull(),
-    providerAccountId: text('provider_account_id').notNull(),
-    refresh_token: text('refresh_token'),
-    access_token: text('access_token'),
-    expires_at: integer('expires_at'),
-    token_type: text('token_type'),
-    scope: text('scope'),
-    id_token: text('id_token'),
-    session_state: text('session_state'),
+    type: text().$type<AdapterAccountType>().notNull(),
+    provider: text().notNull(),
+    providerAccountId: text().notNull(),
+    refresh_token: text(),
+    access_token: text(),
+    expires_at: integer(),
+    token_type: text(),
+    scope: text(),
+    id_token: text(),
+    session_state: text(),
   },
   (account) => ({
     compoundKey: primaryKey({
@@ -49,19 +49,19 @@ export const accounts = pgTable(
 );
 
 export const sessions = pgTable('sessions', {
-  sessionToken: text('session_token').primaryKey(),
-  userId: text('user_id')
+  sessionToken: text().primaryKey(),
+  userId: text()
     .notNull()
     .references(() => users.id, { onDelete: 'cascade' }),
-  expires: timestamp('expires', { mode: 'date' }).notNull(),
+  expires: timestamp({ mode: 'date' }).notNull(),
 });
 
 export const verificationTokens = pgTable(
   'verification_tokens',
   {
-    identifier: text('identifier').notNull(),
-    token: text('token').notNull(),
-    expires: timestamp('expires', { mode: 'date' }).notNull(),
+    identifier: text().notNull(),
+    token: text().notNull(),
+    expires: timestamp({ mode: 'date' }).notNull(),
   },
   (verificationToken) => ({
     compositePk: primaryKey({
@@ -73,16 +73,16 @@ export const verificationTokens = pgTable(
 export const authenticators = pgTable(
   'authenticators',
   {
-    credentialID: text('credential_id').notNull().unique(),
-    userId: text('user_id')
+    credentialID: text().notNull().unique(),
+    userId: text()
       .notNull()
       .references(() => users.id, { onDelete: 'cascade' }),
-    providerAccountId: text('provider_account_id').notNull(),
-    credentialPublicKey: text('credential_public_key').notNull(),
-    counter: integer('counter').notNull(),
-    credentialDeviceType: text('credential_device_type').notNull(),
-    credentialBackedUp: boolean('credential_backed_up').notNull(),
-    transports: text('transports'),
+    providerAccountId: text().notNull(),
+    credentialPublicKey: text().notNull(),
+    counter: integer().notNull(),
+    credentialDeviceType: text().notNull(),
+    credentialBackedUp: boolean().notNull(),
+    transports: text(),
   },
   (authenticator) => ({
     compositePK: primaryKey({
@@ -92,14 +92,14 @@ export const authenticators = pgTable(
 );
 
 export const locations = pgTable('locations', {
-  id: varchar('id', { length: 21 })
+  id: varchar({ length: 21 })
     .$defaultFn(() => nanoid())
     .primaryKey(),
-  placeId: text('place_id').unique().notNull(),
-  name: text('name').notNull(),
-  formattedAddress: text('formatted_address'),
-  createdAt: timestamp('created_at', { mode: 'date' }).defaultNow(),
-  updatedAt: timestamp('updated_at', { mode: 'date' }),
+  placeId: text().unique().notNull(),
+  name: text().notNull(),
+  formattedAddress: text(),
+  createdAt: timestamp({ mode: 'date' }).defaultNow(),
+  updatedAt: timestamp({ mode: 'date' }),
 });
 
 export const contentTypes = ['image', 'video'] as const;
@@ -115,17 +115,13 @@ export const contentStatuses = [
 export type ContentStatus = (typeof contentStatuses)[number];
 
 export const content = pgTable('content', {
-  id: varchar('id', { length: 21 })
+  id: varchar({ length: 21 })
     .$defaultFn(() => nanoid())
     .primaryKey(),
-  type: text('type', { enum: contentTypes }).notNull().default('image'),
-  fileName: text('file_name'),
-  locationId: varchar('location_id', { length: 21 }).references(
-    () => locations.id
-  ),
-  status: text('status', { enum: contentStatuses })
-    .notNull()
-    .default('published'),
-  createdAt: timestamp('created_at', { mode: 'date' }).defaultNow(),
-  updatedAt: timestamp('updated_at', { mode: 'date' }),
+  type: text({ enum: contentTypes }).notNull().default('image'),
+  fileName: text(),
+  locationId: varchar({ length: 21 }).references(() => locations.id),
+  status: text({ enum: contentStatuses }).notNull().default('published'),
+  createdAt: timestamp({ mode: 'date' }).defaultNow(),
+  updatedAt: timestamp({ mode: 'date' }),
 });
